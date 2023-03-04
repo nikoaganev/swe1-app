41a99b4efce712f5f6d454b2eeddca724531e565
Nikola Ganev <ganevniko@hotmail.com>
2023-03-04 18:16:14 -0500

    formatting

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,11 @@
-from django.http import HttpResponse, HttpResponseRedirect
-from django.template import loader
+from django.http import HttpResponseRedirect  # , HttpResponse
+
+# from django.template import loader
 from django.shortcuts import render
 from .models import Choice, Question
-from django.http import Http404
-from django.shortcuts import get_object_or_404, render
+
+# from django.http import Http404
+from django.shortcuts import get_object_or_404  # , render
 from django.urls import reverse
 from django.views import generic
 
